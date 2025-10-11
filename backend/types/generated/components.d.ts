a1e2080d0ad7e11900ed14f3267f3078b6acd875
Brent Edwards <bedwards@inmusicbrands.com>
2025-10-11 17:04:37 +1300

    Update forms

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -70,14 +70,10 @@ export interface SectionsForm extends Struct.ComponentSchema {
     displayName: 'Form';
   };
   attributes: {
-    contactNumber: Schema.Attribute.String;
-    email: Schema.Attribute.String & Schema.Attribute.Required;
     field: Schema.Attribute.Component<'form.field', true>;
-    firstName: Schema.Attribute.String;
     formUniqueName: Schema.Attribute.String &
       Schema.Attribute.Required &
       Schema.Attribute.Unique;
-    lastName: Schema.Attribute.String;
     redirectTo: Schema.Attribute.String & Schema.Attribute.Required;
     submissionType: Schema.Attribute.Enumeration<
       ['SubmitOnce ', 'AllowUpdate', 'AllowMultiple']
